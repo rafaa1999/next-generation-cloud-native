c1c57e5c82fba634c2aa83cc778665cd74c3fe28
rafaa1999 <raferhouma10@gmail.com>
2024-07-30 09:43:57 +0100

    Add book domain

--- src/main/java/com/rafaa/next_generation/NextGenerationApplication.java
+++ src/main/java/com/rafaa/next_generation/NextGenerationApplication.java
@@ -2,6 +2,11 @@ package com.rafaa.next_generation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class NextGenerationApplication {
@@ -11,3 +16,19 @@ public class NextGenerationApplication {
 	}
 
 }
+
+@RestController
+@RequestMapping("/books")
+class BookController{
+
+	@GetMapping
+	List<Book> getAllBooks(){
+		return List.of(
+				new Book(1L,"Game of thrones"),
+				new Book(2L,"The lord of the ring")
+		);
+	}
+
+}
+
+record Book(Long id, String title){}
