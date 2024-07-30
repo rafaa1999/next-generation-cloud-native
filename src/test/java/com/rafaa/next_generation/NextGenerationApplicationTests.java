a70550317722140bbdef4ccd4e220ec7c79a93f8
rafaa1999 <raferhouma10@gmail.com>
2024-07-30 09:31:10 +0100

    Setup project

---
+++ src/test/java/com/rafaa/next_generation/NextGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaa.next_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
