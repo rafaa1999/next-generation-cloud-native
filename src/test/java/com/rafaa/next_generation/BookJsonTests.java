4f54b659cec977f13049b0ef6be02c5db194c1f7
rafaa1999 <raferhouma10@gmail.com>
2024-07-30 10:01:18 +0100

    Test json serialization

---
+++ src/test/java/com/rafaa/next_generation/BookJsonTests.java
@@ -0,0 +1,41 @@
+package com.rafaa.next_generation;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class BookJsonTests {
+
+    @Autowired
+    private JacksonTester<Book> json;
+
+    @Test
+    void testSerialize() throws Exception {
+       var book = new Book(1L,"Game of thrones");
+       var jsonContent = json.write(book);
+       assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
+               .isEqualTo(book.id().intValue());
+       assertThat(jsonContent).extractingJsonPathStringValue("@.title")
+               .isEqualTo(book.title());
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        var book = """
+                    {
+                        "id":1,
+                        "title":"Game of thrones"
+                    }
+                """;
+        assertThat(json.parse(book))
+                .usingRecursiveComparison()
+                .isEqualTo(new Book(1L,"Game of thrones"));
+    }
+
+}
